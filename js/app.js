28ea1ba9a35ba4c0fc2321285dd8e6d0a71e4742
shrey <shreysharan.0408@gmail.com>
2022-04-13 12:12:17 +0530

    Added Title to Notes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 function addToLocalStorage() {
     let textElement = document.getElementById("addtext");
     let text = textElement.value;
-    console.log("In function");
+    let titleElement = document.getElementById("addtitle");
+    let title = titleElement.value;
     if (text != "") {
         let notesArray = []
         if (localStorage.getItem("notes") != null) {
             notesArray = JSON.parse(localStorage.getItem("notes"));
         }
-        notesArray.push(text)
+        let pair = [title, text]
+        notesArray.push(pair)
         localStorage.setItem("notes", JSON.stringify(notesArray));
         textElement.value = "";
+        titleElement.value = "";
     }
 }
-
 function displayNotes() {
     let notes = localStorage.getItem("notes");
     let html = "";
@@ -23,8 +25,8 @@ function displayNotes() {
             html += `
             <div class="noteCard card mx-2 my-2" style="width: 18rem;">
                 <div class="card-body">
-                  <h5 class="card-title">Note ${index + 1}</h5>
-                  <p class="card-text">${element}</p>
+                  <h5 class="card-title">${element[0]}</h5>
+                  <p class="card-text">${element[1]}</p>
                   <button class="btn btn-primary" onclick="deleteNote(${index})">Delete Node</a>
                 </div>
               </div>
@@ -53,11 +55,6 @@ function deleteAllNotes() {
     localStorage.clear();
     displayNotes();
 }
-
-
-
-
-
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", () => {
     addToLocalStorage();
@@ -69,13 +66,14 @@ searchText1.addEventListener("input", () => {
     let noteCards = document.getElementsByClassName("noteCard");
     Array.from(noteCards).forEach(function (element) {
         let cardText = element.getElementsByTagName("p")[0].innerText.toLowerCase();
+        let cardTitle = element.getElementsByTagName("h5")[0].innerText.toLowerCase();
         if (cardText.includes(inputVal)) {
             element.style.display = "block";
-        } else {
+        }else if (cardTitle.includes(inputVal)) {
+            element.style.display = "block";
+        }else {
             element.style.display = "none";
         }
     });
 });
-
-
 displayNotes();
